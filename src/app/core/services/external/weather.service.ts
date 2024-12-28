d889fe6b5a5738b5db2d1ae6f377cecfb992ffa6
Khoa <khoadha9a2.dt@gmail.com>
2024-12-28 21:19:31 +0700

    deploy

--- src/app/core/services/external/weather.service.ts
+++ src/app/core/services/external/weather.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { WeatherData } from '../../models/external/weather-data';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
+  readonly baseUrl = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
 
   getWeatherInfoByLocation(location: string): Observable<WeatherData>{
-    return this.http.get<WeatherData>(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c6a481fb35eeb4b4a44deef4adce2235`);
+    return this.http.get<WeatherData>(`${this.baseUrl}utilities/weather?location=${location}`);
   }
 }
